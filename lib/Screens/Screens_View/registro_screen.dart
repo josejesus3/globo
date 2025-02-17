27dfea7a1e51a837e95b4e30dfec2ab0d083fb2b
josejesus3 <gutierresrayas88@gmail.com>
2025-02-17 15:07:43 -0600

    tabBar

--- lib/Screens/Screens_View/registro_screen.dart
+++ lib/Screens/Screens_View/registro_screen.dart
@@ -2,9 +2,11 @@ import 'dart:io';
 
 import 'package:animate_do/animate_do.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:globo/services/actualizar_registros.dart';
+import 'package:globo/services/select_image.dart';
 import 'package:globo/widget/tabbar_unidades_economicas.dart';
+import 'package:image_picker/image_picker.dart';
 
 class RegistroScreen extends StatefulWidget {
   final String title;
@@ -93,198 +95,123 @@ class _RegistroScreenState extends State<RegistroScreen>
   Widget build(BuildContext context) {
     TabController tabController = TabController(length: 3, vsync: this);
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF202A53),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(
+            Icons.arrow_back_outlined,
+          ),
+          color: Colors.white,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final XFile? image = await selectImage();
+              setState(() {
+                if (image == null) {
+                } else {
+                  setState(() {
+                    imagenUpload = File(image.path);
+                  });
+                }
+              });
+            },
+            icon: Icon(
+              Icons.add_photo_alternate_outlined,
+              size: 30,
+              color: widget.url != ''
+                  ? Colors.white
+                  : imagenUpload != null
+                      ? Colors.white
+                      : Colors.white,
+            ),
+          ),
+        ],
+      ),
       body: CustomScrollView(
         physics: const ClampingScrollPhysics(),
         slivers: [
           _CustomSliverAppBar(
-            imagen: widget.imagen,
+            url: widget.url,
             title: widget.title,
+            imagen: widget.imagen!,
+            imagenUpload: imagenUpload,
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 350),
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(10),
-                ),
-              ),
-              child: SingleChildScrollView(
-                child: Expanded(
+          SliverToBoxAdapter(
+            child: TabBar(
+              controller: tabController,
+              tabs: [
+                Tab(icon: Icon(Icons.description_outlined)),
+                Tab(icon: Icon(Icons.photo_outlined)),
+                Tab(icon: Icon(Icons.contact_phone_outlined)),
+              ],
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey,
+              indicatorColor: Colors.blue,
+            ),
+          ),
+          SliverFillRemaining(
+            child: TabBarView(
+              controller: tabController,
+              children: [
+                // Aquí puedes poner el contenido de cada pestaña
+                RegistroDescripcion(
+                    textFieldDescripcion: _textFieldDescripcion),
+
+                // Para la pestaña de elegir imágenes, asignamos dimensiones específicas:
+                Container(
+                  padding: EdgeInsets.all(16),
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      // Título editable y botón de actualización
-                      Padding(
-                        padding: const EdgeInsets.only(top: 15, left: 35),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Flexible(
-                              child: TextFormField(
-                                style: const TextStyle(fontSize: 25),
-                                controller: _textFieldTitulo,
-                                decoration: const InputDecoration(
-                                  focusedBorder: UnderlineInputBorder(
-                                    borderRadius: BorderRadius.horizontal(),
-                                    borderSide: BorderSide(color: Colors.black),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 15,
-                            ),
-                            Column(
-                              children: [
-                                IconButton(
-                                  color: Colors.orange.shade500,
-                                  onPressed: () {
-                                    setState(() {
-                                      url1 = widget.url;
-
-                                      if (imagenUpload != null) {
-                                        // Si al menos una imagen está presente
-                                        DatabaseOperations.actualizarApp(
-                                          titulo: _textFieldTitulo.text,
-                                          descripcion:
-                                              _textFieldDescripcion.text,
-                                          telefono: _textFieldTelefono.text,
-                                          direccion: _textFieldDireccion.text,
-                                          horarioEntrada:
-                                              _textFieldhorarioEntrada.text,
-                                          horarioSalida:
-                                              _textFieldhorarioSalida.text,
-                                          imagenUpload: imagenUpload!,
+                      const Text('Elegir Imagenes'),
+                      IconButton(
+                        onPressed: widget.galeriaUrl != ''
+                            ? () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => GaleriaImagen(
                                           contactKey: widget.contactKey,
-                                          databaseReference: databaseReference,
-                                          context: context,
-                                          url: url1!,
-                                          facebook: widget.facebook!,
-                                          instagram: widget.instagram!,
-                                          whatsapp: _textFieldWhatsApp.text,
-                                        );
-                                      } else {
-                                        // Si ninguna imagen está presente
-                                        DatabaseOperations
-                                            .actualizarAppSinImagen(
-                                          titulo: _textFieldTitulo.text,
-                                          descripcion:
-                                              _textFieldDescripcion.text,
-                                          telefono: _textFieldTelefono.text,
-                                          direccion: _textFieldDireccion.text,
-                                          horarioEntrada:
-                                              _textFieldhorarioEntrada.text,
-                                          horarioSalida:
-                                              _textFieldhorarioSalida.text,
-                                          contactKey: widget.contactKey,
-                                          databaseReference: databaseReference,
-                                          context: context,
-                                          url: url1!,
-                                          facebook: _textFieldFacebook.text,
-                                          instagram: _textFieldInstagram.text,
-                                          whatsapp: _textFieldWhatsApp.text,
-                                        );
-                                      }
-                                    });
-                                  },
-                                  icon: const Icon(
-                                    Icons.edit_document,
-                                    size: 35,
-                                    color: Color.fromARGB(255, 228, 149, 52),
-                                  ),
-                                ),
-                                const Text('Actualizar'),
-                              ],
-                            ),
-                            const SizedBox(
-                              width: 20,
-                            ),
-                          ],
+                                          galeriaUrl: galeria),
+                                    ));
+                              }
+                            : () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => GaleriaSinUrl(
+                                          contactKey: widget.contactKey),
+                                    ));
+                              },
+                        icon: const Icon(
+                          Icons.add_photo_alternate_outlined,
+                          size: 50,
                         ),
-                      ),
-
-                      // Pestañas para descripción, galería y contacto
-                      TabBar(
-                        controller: tabController,
-                        tabs: const [
-                          Tab(icon: Icon(Icons.description_outlined)),
-                          Tab(icon: Icon(Icons.photo_outlined)),
-                          Tab(icon: Icon(Icons.contact_phone_outlined)),
-                        ],
-                      ),
-
-                      // Contenido de las pestañas
-                      SizedBox(
-                        width: double.infinity,
-                        height: 550,
-                        child: TabBarView(controller: tabController, children: [
-                          // Pestaña de descripción
-                          RegistroDescripcion(
-                              textFieldDescripcion: _textFieldDescripcion),
-
-                          // Pestaña de galería de imágenes
-                          Stack(
-                            children: [
-                              Positioned(
-                                top: 100,
-                                left: 150,
-                                child: Column(
-                                  children: [
-                                    const Text('Elegir Imagenes'),
-                                    IconButton(
-                                      onPressed: widget.galeriaUrl != ''
-                                          ? () {
-                                              Navigator.push(
-                                                  context,
-                                                  MaterialPageRoute(
-                                                    builder: (context) =>
-                                                        GaleriaImagen(
-                                                            contactKey: widget
-                                                                .contactKey,
-                                                            galeriaUrl:
-                                                                galeria),
-                                                  ));
-                                            }
-                                          : () {
-                                              Navigator.push(
-                                                  context,
-                                                  MaterialPageRoute(
-                                                    builder: (context) =>
-                                                        GaleriaSinUrl(
-                                                            contactKey: widget
-                                                                .contactKey),
-                                                  ));
-                                            },
-                                      icon: const Icon(
-                                        Icons.add_photo_alternate_outlined,
-                                        size: 50,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-
-                          // Pestaña de contacto
-                          RegistroContacto(
-                            textFieldDireccion: _textFieldDireccion,
-                            textFieldHorarioSalida: _textFieldhorarioSalida,
-                            textFieldHorarioEntrada: _textFieldhorarioEntrada,
-                            textFieldtelefono: _textFieldTelefono,
-                            textFieldFacebook: _textFieldFacebook,
-                            textFieldInstagram: _textFieldInstagram,
-                            textFieldWhatsApp: _textFieldWhatsApp,
-                          ),
-                        ]),
                       ),
                     ],
                   ),
                 ),
-              ),
+
+                // Para el contenido de contacto, también puedes agregar dimensiones:
+                Container(
+                  padding: EdgeInsets.all(16),
+                  child: RegistroContacto(
+                    textFieldDireccion: _textFieldDireccion,
+                    textFieldHorarioSalida: _textFieldhorarioSalida,
+                    textFieldHorarioEntrada: _textFieldhorarioEntrada,
+                    textFieldtelefono: _textFieldTelefono,
+                    textFieldFacebook: _textFieldFacebook,
+                    textFieldInstagram: _textFieldInstagram,
+                    textFieldWhatsApp: _textFieldWhatsApp,
+                  ),
+                ),
+              ],
             ),
-          ),
+          )
         ],
       ),
     );
@@ -292,10 +219,17 @@ class _RegistroScreenState extends State<RegistroScreen>
 }
 
 class _CustomSliverAppBar extends StatefulWidget {
-  final String? imagen;
+  final String? url;
   final String title;
+  final String imagen;
+  final File? imagenUpload;
+
   const _CustomSliverAppBar(
-      {super.key, required this.imagen, required this.title});
+      {super.key,
+      required this.url,
+      required this.title,
+      required this.imagen,
+      this.imagenUpload});
 
   @override
   State<_CustomSliverAppBar> createState() => _CustomSliverAppBarState();
@@ -305,32 +239,61 @@ DatabaseReference databaseReference =
     FirebaseDatabase.instance.ref('UnidadesEconomicas');
 
 class _CustomSliverAppBarState extends State<_CustomSliverAppBar> {
+  final TextEditingController _textFieldTitulo = TextEditingController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _textFieldTitulo.text = widget.title;
+  }
+
   @override
   Widget build(BuildContext context) {
     final sized = MediaQuery.of(context).size;
     return SliverAppBar(
+      leading: Container(),
       backgroundColor: Colors.black,
-      expandedHeight: sized.height * 0.7,
+      expandedHeight: sized.height * 0.65,
       foregroundColor: Colors.white,
       flexibleSpace: FlexibleSpaceBar(
-        title: Text(
-          widget.title,
-          style: TextStyle(fontSize: 20),
-          textAlign: TextAlign.start,
+        title: TextFormField(
+          style: const TextStyle(fontSize: 14),
+          controller: _textFieldTitulo,
+          decoration: const InputDecoration(
+            focusedBorder: UnderlineInputBorder(
+              borderRadius: BorderRadius.horizontal(),
+              borderSide: BorderSide(color: Colors.black),
+            ),
+          ),
         ),
         titlePadding: const EdgeInsets.only(left: 10, right: 20, bottom: 30),
         background: Stack(
           children: [
             SizedBox.expand(
-              child: Image.network(
-                widget.imagen!,
-                fit: BoxFit.cover,
-                loadingBuilder: (context, child, loadingProgress) {
-                  if (loadingProgress != null) return const SizedBox();
-                  return FadeIn(child: child);
-                },
-              ),
+              child: widget.url != ''
+                  ? Image.network(
+                      widget.url!,
+                      fit: BoxFit.cover,
+                      loadingBuilder: (context, child, loadingProgress) {
+                        if (loadingProgress != null) return const SizedBox();
+                        return FadeIn(child: child);
+                      },
+                    )
+                  : Container(
+                      color: Colors.white,
+                      child: Image.asset(
+                        widget.imagen,
+                      ),
+                    ),
             ),
+            widget.imagenUpload != null
+                ? Image.file(
+                    widget.imagenUpload!,
+                    width: sized.width * 1.0,
+                    height: sized.height * 0.7,
+                    fit: BoxFit.cover,
+                  )
+                : Container(),
           ],
         ),
       ),
